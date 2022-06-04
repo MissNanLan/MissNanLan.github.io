47f366ff7745911a832bb8db57b6d152ea2dbe58
nanlan <nanlan@nanlandeMacBook-Pro.local>
2022-06-04 12:02:54 +0800

    增加发布脚本

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "===========  start deploy  ==========="
+
+/bin/echo -n "输入改动信息: "
+read change 
+
+git checkout master
+git add .
+git commit -m $change
+git push 
+
+
+git checkout pages
+hexo clean
+hexo g
+hexo d
+
+
+
+
+echo "===========  end  deploy  ==========="
+
+exit 0 
